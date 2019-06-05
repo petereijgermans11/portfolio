58c9104220f04d658d4da99f9c1a23871eb207a9
Eijgermans, P.B.J. (Peter) <peter.eijgermans@devolksbank.nl>
2019-06-05 10:23:33 +0200

    Updates

---
+++ precache-manifest.0506f6bd23ea4c8c0cdebdd5945ae790.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "e7359e2f049a124d957d",
+    "url": "/portfolio/static/css/main.4362fb7c.chunk.css"
+  },
+  {
+    "revision": "e7359e2f049a124d957d",
+    "url": "/portfolio/static/js/main.e7359e2f.chunk.js"
+  },
+  {
+    "revision": "0fad4c0f10d57b530fe4",
+    "url": "/portfolio/static/js/runtime~main.0fad4c0f.js"
+  },
+  {
+    "revision": "5ec21d738fba382f2658",
+    "url": "/portfolio/static/js/2.5ec21d73.chunk.js"
+  },
+  {
+    "revision": "6b48052fe1f8a15de7d06392565e7ffd",
+    "url": "/portfolio/index.html"
+  }
+];
