e4bbc539a914822196caa2d9ff78961ecd937203
Eijgermans, P.B.J. (Peter) <peter.eijgermans@rabobank.nl>
2020-02-14 11:28:07 +0100

    Updates

--- precache-manifest.44df7b81047151b0d3af517c96850ba1.js
+++ precache-manifest.44df7b81047151b0d3af517c96850ba1.js
@@ -1,11 +1,11 @@
 self.__precacheManifest = [
   {
-    "revision": "7ecd5045d022470ce8f5",
+    "revision": "6103121d41d881bab174",
     "url": "/portfolio/static/css/main.f6f94667.chunk.css"
   },
   {
-    "revision": "7ecd5045d022470ce8f5",
-    "url": "/portfolio/static/js/main.7ecd5045.chunk.js"
+    "revision": "6103121d41d881bab174",
+    "url": "/portfolio/static/js/main.6103121d.chunk.js"
   },
   {
     "revision": "0fad4c0f10d57b530fe4",
@@ -16,7 +16,7 @@ self.__precacheManifest = [
     "url": "/portfolio/static/js/2.5ec21d73.chunk.js"
   },
   {
-    "revision": "97b88cf52021a8d85c285c481e4cd684",
+    "revision": "9c5f76723477069307baae76b83cf37c",
     "url": "/portfolio/index.html"
   }
 ];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/portfolio/precache-manifest.08d6d8821c803c9bfece04ecaa6aa943.js"
+  "/portfolio/precache-manifest.44df7b81047151b0d3af517c96850ba1.js"
 );
 
 workbox.clientsClaim();
