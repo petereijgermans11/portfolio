33d668fcf9f99a566cfbee3e7d5fffe99fb099bf
Eijgermans, P.B.J. (Peter) <p.eijgermans11@gmail.com>
2021-01-16 16:13:00 +0100

    Updates

--- precache-manifest.f890258b0bd1b68a6db77a3f0db75786.js
+++ precache-manifest.f890258b0bd1b68a6db77a3f0db75786.js
@@ -1,11 +1,11 @@
 self.__precacheManifest = [
   {
-    "revision": "cf7fe39f9be2fd834b8e",
+    "revision": "16f1fbcf3edcefa6cfe0",
     "url": "/portfolio/static/css/main.f6f94667.chunk.css"
   },
   {
-    "revision": "cf7fe39f9be2fd834b8e",
-    "url": "/portfolio/static/js/main.cf7fe39f.chunk.js"
+    "revision": "16f1fbcf3edcefa6cfe0",
+    "url": "/portfolio/static/js/main.16f1fbcf.chunk.js"
   },
   {
     "revision": "0fad4c0f10d57b530fe4",
@@ -16,7 +16,7 @@ self.__precacheManifest = [
     "url": "/portfolio/static/js/2.5ec21d73.chunk.js"
   },
   {
-    "revision": "c4221a5a32f54532752b07035bb3dc66",
+    "revision": "4c95f75f372458e506d6e8fd9c82e111",
     "url": "/portfolio/index.html"
   }
 ];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/portfolio/precache-manifest.af33e971670df6c14fe76d0cb7af4453.js"
+  "/portfolio/precache-manifest.f890258b0bd1b68a6db77a3f0db75786.js"
 );
 
 workbox.clientsClaim();
