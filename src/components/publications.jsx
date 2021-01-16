56b4d1f3c0a85b5d04a73cb85164aea92dbde38f
Eijgermans, P.B.J. (Peter) <p.eijgermans11@gmail.com>
2021-01-16 16:16:14 +0100

    added talk about Vue and React for @JSandFriendsConf

--- src/components/publications.jsx
+++ src/components/publications.jsx
@@ -20,7 +20,7 @@ export default class About extends Component {
                         <h3>PWA Tutorial: How to Build a Progressive Web App (PWA) with Face Recognition and Speech Recognition (November 2020)</h3>
                         <p>In this tutorial, I will discuss advanced PWA features that provide access to your hardware APIs. We are going to build an app with Face Recognition and Speech Recognition. This is now possible on the Web!
                         </p>
-                        <a href="https://dzone.com/articles/tutorial-how-to-build-a-pwa-with-face-and-speechrecognition" el="noopener noreferrer" target="_blank" class="btn btn-primary">Check It Out</a>
+                        <a href="https://dzone.com/articles/tutorial-how-to-build-a-pwa-with-face-and-speechrecognition" rel="noopener noreferrer" target="_blank" class="btn btn-primary">Check It Out</a>
                     </div>
                 </div>
             </div>  
@@ -31,7 +31,7 @@ export default class About extends Component {
                         <h3>Hooks by Example: Convert a Tesla Battery Range Calculator to Functional Components with React Hooks (October 2020)</h3>
                         <p>In order to learn React hooks more thoroughly I converted an existing React-app from React Class Components to one with just React Functional Components.
                         </p>
-                        <a href="https://dzone.com/articles/steps-to-convert-the-react-tesla-battery-range-calc-to-func-comp-with-hooks" el="noopener noreferrer" target="_blank" class="btn btn-primary">Check It Out</a>
+                        <a href="https://dzone.com/articles/steps-to-convert-the-react-tesla-battery-range-calc-to-func-comp-with-hooks" rel="noopener noreferrer" target="_blank" class="btn btn-primary">Check It Out</a>
                     </div>
                 </div>
             </div>
@@ -42,7 +42,7 @@ export default class About extends Component {
                         <h3>PWA Tutorial: Enable Background Sync, Media Capture, and Geolocation APIs in Your PWA (September 2020)</h3>
                         <p>In this tutorial, I will discuss advanced PWA features that provide access to your hardware APIs. We are going to build an app for making selfies with the Media Capture API and posting selfies with the BackgroundSync Api.
                         </p>
-                        <a href="https://dzone.com/articles/how-to-build-a-progressive-selfies-web-app" el="noopener noreferrer" target="_blank" class="btn btn-primary">Check It Out</a>
+                        <a href="https://dzone.com/articles/how-to-build-a-progressive-selfies-web-app" rel="noopener noreferrer" target="_blank" class="btn btn-primary">Check It Out</a>
                     </div>
                 </div>
             </div>
@@ -53,7 +53,7 @@ export default class About extends Component {
                         <h3>PWA Tutorial: How to Build a Progressive Web App (Februari 2020)</h3>
                         <p>In this article/tutorial, we discuss some basics behind building PWAs and then provide a comprehensive tutorial on creating your first one.
                         </p>
-                        <a href="https://dzone.com/articles/how-to-build-a-progressive-web-app-pwa-with-javasc" el="noopener noreferrer" target="_blank" class="btn btn-primary">Check It Out</a>
+                        <a href="https://dzone.com/articles/how-to-build-a-progressive-web-app-pwa-with-javasc" rel="noopener noreferrer" target="_blank" class="btn btn-primary">Check It Out</a>
                     </div>
                 </div>
             </div>
@@ -64,7 +64,7 @@ export default class About extends Component {
                         <h3>Vue.js Tutorial: Build a Tesla Battery Range Calculator (September 2019)</h3>
                         <p>In this tutorial, we get started with Vue.js, an easy-to-understand JavaScript framework.
                         </p>
-                        <a href="https://dzone.com/articles/build-a-tesla-battery-range-calculator-yourself-it" el="noopener noreferrer" target="_blank" class="btn btn-primary">Check It Out</a>
+                        <a href="https://dzone.com/articles/build-a-tesla-battery-range-calculator-yourself-it" rel="noopener noreferrer" target="_blank" class="btn btn-primary">Check It Out</a>
                     </div>
                 </div>
             </div>
@@ -75,7 +75,7 @@ export default class About extends Component {
                         <h3>Building Mobile Apps With Angular or Vue.js and NativeScript</h3>
                         <p>In this article, we go over to quickly make mobile applications using NativeScript plus Angular and NativeScript plus Vue.js.
                         </p>
-                        <a href="https://dzone.com/articles/building-mobile-apps-with-angular-and-nativescript" el="noopener noreferrer"target="_blank" class="btn btn-primary">Check It Out</a>
+                        <a href="https://dzone.com/articles/building-mobile-apps-with-angular-and-nativescript" rel="noopener noreferrer"target="_blank" class="btn btn-primary">Check It Out</a>
                     </div>
                 </div>
             </div>
@@ -85,7 +85,7 @@ export default class About extends Component {
                     <div className="desc">
                         <h3>Microservices? Yes, but No Spaghetti, Please!</h3>
                         <p>Microservices bring many benefits, but make sure you're not adding more complexity than you need and getting bogged down with spaghetti code.</p>
-                        <a href="https://dzone.com/articles/microservices-yes-but-no-spaghetti-please" el="noopener noreferrer"target="_blank" class="btn btn-primary">Check It Out</a>
+                        <a href="https://dzone.com/articles/microservices-yes-but-no-spaghetti-please" rel="noopener noreferrer"target="_blank" class="btn btn-primary">Check It Out</a>
                     </div>
                 </div>
             </div>
@@ -95,7 +95,7 @@ export default class About extends Component {
                     <div className="desc">
                         <h3>Create Your Own Yeoman Generator</h3>
                         <p>Sometimes you may have some specific setup that you like to use in your own projects. Because you don’t want to reinvent your own wheel in every project, it makes sense to abstract all the boilerplate into your own generator. In this situation you can build your own Yeoman generator. This will kick-start your projects.</p>
-                        <a href="https://dzone.com/articles/create-your-own-yeoman" el="noopener noreferrer"target="_blank" class="btn btn-primary">Check It Out</a>
+                        <a href="https://dzone.com/articles/create-your-own-yeoman" rel="noopener noreferrer"target="_blank" class="btn btn-primary">Check It Out</a>
                     </div>
                 </div>
             </div>
